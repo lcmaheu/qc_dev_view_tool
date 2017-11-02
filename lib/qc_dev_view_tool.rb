60287ec0002ca78579969f3dfaa171497092bcf3
lcmaheu <lcmaheu@gmail.com>
2017-11-02 22:03:14 +0000

    Dev tool initial commit

---
+++ lib/qc_dev_view_tool.rb
@@ -0,0 +1,6 @@
+require "qc_dev_view_tool/version"
+require "qc_dev_view_tool/renderer"
+
+module QcDevViewTool
+  # Your code goes here...
+end
---
+++ lib/qc_dev_view_tool/version.rb
@@ -0,0 +1,3 @@
+module QcDevViewTool
+  VERSION = "0.1.0"
+end
